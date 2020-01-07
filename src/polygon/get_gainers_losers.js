2e1fa3821525c9502c9a6583355afe629bf4d3ca
tyler-tower <tyler.munyon@metals.com>
2020-01-07 13:14:32 -0800

    gainers losers

--- src/polygon/get_gainers_losers.js
+++ src/polygon/get_gainers_losers.js
@@ -1,18 +1,55 @@
 import axios from 'axios';
+import * as util from 'util' // has no default export
+import { inspect } from 'util' // or directly
+
+function simpleStringify (object){
+    var simpleObject = {};
+    for (var prop in object ){
+        if (!object.hasOwnProperty(prop)){
+            continue;
+        }
+        if (typeof(object[prop]) == 'object'){
+            continue;
+        }
+        if (typeof(object[prop]) == 'function'){
+            continue;
+        }
+        simpleObject[prop] = object[prop];
+    }
+    return JSON.stringify(simpleObject); // returns cleaned up JSON
+};
+
 
 export function getGainers() {
-    let gainers = axios.get(`https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/losers?apiKey=${process.env.POLYGON_API_KEY}`)
+    //function getGainers() {
+    let gainers = axios.get(`https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/gainers?apiKey=${process.env.POLYGON_API_KEY}`)
         .then(function(res) {
-            return res
+        return(res)
+
     }).catch(function(err){
-            return res
+        return err
     })
 
-    return gainers
+    return gainers.then((data) => data.data)
 }
+
+export function getLosers() {
+    //function getGainers() {
+    let losers = axios.get(`https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/losers?apiKey=${process.env.POLYGON_API_KEY}`)
+        .then(function(res) {
+        return res
+
+    }).catch(function(err){
+        return err
+    })
+
+    return losers.then((data) => data.data)
+}
+
+//getGainers().then(data => console.log(Object.keys(data.data.tickers)))
 
 //export function getLosers() {
 
-    //}
+//}
 //
 
