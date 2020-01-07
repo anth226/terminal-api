c61765043f3d383d2643b54af2c51e83c3a172b8
tyler-tower <tyler.munyon@metals.com>
2020-01-06 20:32:22 -0800

    Grab port and frontend url from .env

--- src/index.js
+++ src/index.js
@@ -16,14 +16,14 @@ import bodyParser from 'body-parser';
 import Stripe from 'stripe';
 
 /*
-~~~~~~Configuration Stuff~~~~~~
-*/
+   ~~~~~~Configuration Stuff~~~~~~
+   */
 
 // init firebase
 const serviceAccount = require("../tower-93be8-firebase-adminsdk-o954n-87d13d583d.json");
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://tower-93be8.firebaseio.com"
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://tower-93be8.firebaseio.com"
 });
 
 // init stripe
@@ -38,17 +38,17 @@ const indexAPI = new intrinioSDK.IndexApi();
 // configure secure cookies
 const expiresIn = 60 * 60 * 24 * 5 * 1000;
 const cookieParams = {
-  maxAge: expiresIn,
-  httpOnly: true,  // dont let browser javascript access cookie ever
-  secure: true, // only use cookie over https
-  ephemeral: true // delete this cookie while browser close
+    maxAge: expiresIn,
+    httpOnly: true,  // dont let browser javascript access cookie ever
+    secure: true, // only use cookie over https
+    ephemeral: true // delete this cookie while browser close
 }
 
 // configure CORS
 var corsOptions = {
-  origin: 'http://localhost:3001',
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  credentials: true,
+    origin: 'http://' + process.env.FRONTEND_URL + ':' + process.env.FRONTEND_PORT,
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    credentials: true,
 }
 
 // set up middlewares
@@ -59,40 +59,40 @@ app.use(express.json());
 
 
 /*
-~~~~~~Middlewares~~~~~~
-*/
+   ~~~~~~Middlewares~~~~~~
+   */
 
 function checkAuth(req, res, next) {
-  if (req.cookies.access_token && req.cookies.access_token.split(' ')[0] === 'Bearer') { // Authorization: Bearer g1jipjgi1ifjioj
-      // Handle token presented as a Bearer token in the Authorization header
-      const session = req.cookies.access_token.split(' ')[1];
-      admin.auth().verifySessionCookie(
-        session, true /** checkRevoked */)
-        .then((decodedClaims) => {
-          req.user = decodedClaims.name
-          next();
-        })
-        .catch(error => {
-          // Session is unavailable or invalid. Force user to login.
-          res.status(403).send('Unauthorized');
-        });
+    if (req.cookies.access_token && req.cookies.access_token.split(' ')[0] === 'Bearer') { // Authorization: Bearer g1jipjgi1ifjioj
+        // Handle token presented as a Bearer token in the Authorization header
+        const session = req.cookies.access_token.split(' ')[1];
+        admin.auth().verifySessionCookie(
+            session, true /** checkRevoked */)
+            .then((decodedClaims) => {
+                req.user = decodedClaims.name
+                next();
+            })
+            .catch(error => {
+                // Session is unavailable or invalid. Force user to login.
+                res.status(403).send('Unauthorized');
+            });
     } else {
-      res.status(403).send('Unauthorized');
+        res.status(403).send('Unauthorized');
     }
 
 }
 
 
 /*
-~~~~~~Routes~~~~~~
+   ~~~~~~Routes~~~~~~
 
 
-v  if(decodedToken.email_verified == false) {
+   v  if(decodedToken.email_verified == false) {
 
-    res.json({ status: "verify_email", message: "Please verify your email address: " + decodedToken.email });
+   res.json({ status: "verify_email", message: "Please verify your email address: " + decodedToken.email });
 
-  } else
-*/
+   } else
+   */
 
 // index
 app.get('/', async (req, res) => {
@@ -101,74 +101,74 @@ app.get('/', async (req, res) => {
 
 // exchange firebase token
 app.post('/getToken', async (req, res) => {
-  const idToken = req.body.token.toString();
+    const idToken = req.body.token.toString();
 
-  admin.auth().verifyIdToken(idToken)
-  .then((decodedToken) => {
+    admin.auth().verifyIdToken(idToken)
+    .then((decodedToken) => {
 
-   if (new Date().getTime() / 1000 - decodedToken.auth_time < 5 * 60) {
+        if (new Date().getTime() / 1000 - decodedToken.auth_time < 5 * 60) {
 
-      admin.auth().createSessionCookie(idToken, {expiresIn}).then((sessionToken) => {
+            admin.auth().createSessionCookie(idToken, {expiresIn}).then((sessionToken) => {
 
-        res.cookie('access_token', 'Bearer ' + sessionToken, cookieParams).end(JSON.stringify({status: "success"}));
+                res.cookie('access_token', 'Bearer ' + sessionToken, cookieParams).end(JSON.stringify({status: "success"}));
 
-      }).catch(error => {
-        res.json({status:"error", message: error + " Unable to create session token, please try logging in again."});
-      });
+            }).catch(error => {
+                res.json({status:"error", message: error + " Unable to create session token, please try logging in again."});
+            });
 
-    } else {
-      res.json({status: "error", message: "Your login session has expired, please try logging in again."});
-    }
+        } else {
+            res.json({status: "error", message: "Your login session has expired, please try logging in again."});
+        }
 
-  }).catch(error => {
-    res.json({status: "error", message: "Unable to verify your login information, please try logging in again."});
-  })
+    }).catch(error => {
+        res.json({status: "error", message: "Unable to verify your login information, please try logging in again."});
+    })
 });
 
 app.use('/payment', checkAuth)
 app.post('/payment', async (req, res) => {
 
-  // verify firebase id token here and don't use checkAuth middleware?
+    // verify firebase id token here and don't use checkAuth middleware?
 
-  const customer = await stripe.customers.create({
+    const customer = await stripe.customers.create({
 
-    payment_method: req.body.payment_method,
-    email: req.body_email,
-    invoice_settings: {
-      default_payment_method: req.body.payment_method,
-    },
+        payment_method: req.body.payment_method,
+        email: req.body_email,
+        invoice_settings: {
+            default_payment_method: req.body.payment_method,
+        },
 
-  }).catch(error => {
+    }).catch(error => {
 
-    res.json({status: "error"});
-    return
+        res.json({status: "error"});
+        return
 
-  });
+    });
 
-  const subscription = await stripe.subscriptions.create({
+    const subscription = await stripe.subscriptions.create({
 
-    customer: customer.id,
-    items: [{ plan: "prod_GTx1D3iN163Dw5" }],
-    expand: ["latest_invoice.payment_intent"]
+        customer: customer.id,
+        items: [{ plan: "prod_GTx1D3iN163Dw5" }],
+        expand: ["latest_invoice.payment_intent"]
 
-  }).catch(error => {
+    }).catch(error => {
 
-    res.json({status: "error"});
-    return
+        res.json({status: "error"});
+        return
 
-  });
+    });
 
-  // make db insert call with customer id and subscription id attached to this firebase user id
+    // make db insert call with customer id and subscription id attached to this firebase user id
 
 });
 
 // auth check
 app.use('/profile', checkAuth)
 app.get('/profile', function(req, res, next) {
-  axios.get(`https://jsonplaceholder.typicode.com/users`)
-      .then(resp => {
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+        .then(resp => {
         res.json(resp.data);
-      })
+    })
 });
 
 // general
@@ -241,5 +241,5 @@ app.get('/index-historical/:symbol', async (req, res) => {
 });
 
 app.listen(process.env.PORT, () =>
-    console.log(`listening on ${process.env.PORT}`)
-);
+           console.log(`listening on ${process.env.PORT}`)
+          );
