b854aa722818e97ec3b416ac47aa700923fff1bc
Reed Kilpatrick <reedk865@gmail.com>
2020-01-13 18:14:10 -0800

    securities screener wip

---
+++ src/intrinio/screener.js
@@ -0,0 +1,37 @@
+import intrinioSDK from 'intrinio-sdk';
+import axios from 'axios';
+
+export async function screen(intrinioApi, params) {
+
+  const url = `https://api-v2.intrinio.com/securities/screen?order_column=marketcap&order_direction=asc&api_key=${process.env.INTRINIO_API_KEY_PROD}`;
+  const body = {
+    "operator": "AND",
+    "clauses": [
+      //
+      // {
+      //   "field": "pricetoearnings",
+      //   "operator": "lt",
+      //   "value": "3"
+      // },
+      {
+        "field": "sector",
+        "operator": "eq",
+        "value": "Consumer Goods"
+      },
+      {
+        "field": "marketcap",
+        "operator": "gt",
+        "value": "100000000"
+      },
+    ]
+  }
+
+  let res = axios.post(url, body)
+    .then(function(data) {
+      return data;
+    }).catch(function(err) {
+      return err;
+    })
+
+  return res.then((data) => data.data)
+}
