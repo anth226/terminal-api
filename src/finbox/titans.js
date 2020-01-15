e59da196e09c80be2052e7ce7a9485baa471ae08
tyler-tower <tyler.munyon@metals.com>
2020-01-15 13:31:31 -0800

    titans portfolios w filters

--- src/finbox/titans.js
+++ src/finbox/titans.js
@@ -1,20 +1,28 @@
 import axios from 'axios';
 
-export async function getPortfolios(investor_type) {
-    let portfolio = axios.post('https://makeshift.finbox.com/v4/ideas/query', {
-        filters: {
-            investor_types: ["Billionaire"],
-            limit: 100,
-            skip: 0
-        }
-    })
+export async function getPortfolios(investor_type, ...sectors) {
+    let sectorsArr = sectors[0].split(",")
+    let cleanInvestorType = investor_type.charAt(0).toUpperCase() + investor_type.substring(1);
+    let cleanSectors = [];
+
+    for (let s of sectorsArr) {
+        let capitalized = s.charAt(0).toUpperCase() + s.substring(1)
+        cleanSectors.push(capitalized)
+    }
+
+    let paramFilters = {"filters":{"investor_types":[cleanInvestorType],"sectors": cleanSectors},"limit":30,"skip":0}
+
+    let portfolios = axios.post('https://makeshift.finbox.com/v4/ideas/query', paramFilters)
     .then(function(res) {
+        console.log(res)
         return res
     }).catch(function(err) {
+        console.log(err)
         return err
     })
 
-    return portfolio.then((data) => data.data)
+    // console.log(portfolios.then((data) => data.data))
+    return portfolios.then((data) => data.data)
 
 }
 // {"filters":{"investor_types":["Activist Investor"]},"limit":30,"skip":0}
