37900345c68fb493ae8f392cde1f0fa00e018562
Reed Kilpatrick <reedk865@gmail.com>
2020-01-24 13:04:27 -0800

    bootstrap trending.json

--- src/stocktwits/get_trending.js
+++ src/stocktwits/get_trending.js
@@ -1,16 +1,22 @@
 import axios from 'axios';
 import fs from 'fs';
 
+const path = './src/stocktwits/trending.json';
+
 export async function getTrending()  {
-  let lastTrending = JSON.parse(fs.readFileSync('./src/stocktwits/trending.json'));
-  let currentEpoch = Math.round(Date.now() / 1000);
-  if(currentEpoch - lastTrending.fetch_time < 3600) {
-    return lastTrending;
-  } else {
-    const result = await axios.get("https://api.stocktwits.com/api/2/trending/symbols.json");
-    let data = result.data;
-    data.fetch_time = currentEpoch;
-    fs.writeFileSync('./src/stocktwits/trending.json', JSON.stringify(data))
-    return data;
+  const currentEpoch = Math.round(Date.now() / 1000);
+
+  if(fs.existsSync(path)) {
+    let lastTrending = JSON.parse(fs.readFileSync(path));
+    if(currentEpoch - lastTrending.fetch_time < 3600) {
+      return lastTrending;
+    }
   }
+
+  const result = await axios.get("https://api.stocktwits.com/api/2/trending/symbols.json");
+  let data = result.data;
+  data.fetch_time = currentEpoch;
+  fs.writeFileSync('./src/stocktwits/trending.json', JSON.stringify(data))
+  
+  return data;
 }
