913ffa78b1f780d7c67b0ddc8df32420355cf8e4
Reed Kilpatrick <reedk865@gmail.com>
2020-01-30 17:18:43 -0800

    find similar companies by ticker

--- src/intrinio/screener.js
+++ src/intrinio/screener.js
@@ -66,34 +66,46 @@ export async function highestYields(intrinioApi) {
     });
 
   return res.then((data) => data.data)
-}
+}*/
 
 export async function similarCompanies(ticker, intrinioApi) {
+  // get sector & industry category
+  const res = await Promise.all([intrinioApi.getSecurityDataPointText(ticker, "sector"), intrinioApi.getSecurityDataPointText(ticker, "industry_category")].map(p => p.catch(e => e)));
+  if(res[0] instanceof Error || res[1] instanceof Error) {
+    return [];
+  }
 
-  const url = `https://api-v2.intrinio.com/securities/screen?order_column=trailing_dividend_yield&order_direction=desc&page_size=250&api_key=${process.env.INTRINIO_API_KEY_PROD}`;
+  const sector = res[0].replace(/['"]+/g, '');
+  const industryCategory = res[1].replace(/['"]+/g, '');
+
+  const url = `https://api-v2.intrinio.com/securities/screen?order_column=marketcap&order_direction=desc&page_size=50&api_key=${process.env.INTRINIO_API_KEY_PROD}`;
   const body = {
   "operator": "AND",
   "clauses": [
       {
         "field": "marketcap",
         "operator": "gt",
-        "value": "1000000000"
+        "value": "0"
       },
       {
-        "field": "trailing_dividend_yield",
-        "operator": "gt",
-        "value": "0"
-      }
+        "field": "sector",
+        "operator": "eq",
+        "value": sector,
+      },
+      {
+        "field": "industry_category",
+        "operator": "eq",
+        "value": industryCategory,
+      },
     ]
   }
 
-  let res = axios.post(url, body)
+  let result = axios.post(url, body)
     .then(function(data) {
-      console.log(data);
-      return data;
+      return data.data.filter(obj => obj.security.ticker != ticker);
     }).catch(function(err) {
       return err;
     });
 
-  return res.then((data) => data.data)
-}*/
+  return result.then((data) => data)
+}
