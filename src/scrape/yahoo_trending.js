3ea6811798209a159bbf539f3aef5317929a336d
linuxandchill <tyler.munyon@metals.com>
2020-02-28 11:40:22 -0800

    trending scrape

--- src/scrape/yahoo_trending.js
+++ src/scrape/yahoo_trending.js
@@ -15,36 +15,4 @@ export async function getTrending() {
       }
  ;
 };
-    // $('div[class=".yfinlist-table"]').each(function (index, element){
-    //     trending.push($(element).find('table').find("tr").find('[class~="data-row"]'));
-    //     // [data-test~="foo"]
-    // });
-
-    // $('div[id="list"]').find('div > div > a').each(function (index, element) {
-    //     list.push($(element).attr('href'));
-    //   });
-
-    // $('[data-yaft-module=tdv2-applet-trending_tickers_title]').each(function(index, element) {
-    //     console.log($(element))
-    //     trending.push($(element))
-    // });
-
-    // return trending;
-
-    // data-yaft-module="tdv2-applet-trending_tickers_title"
-
-    // $('.gainers li').each(function (index, element){
-    //     // console.log($(element).find('.quote-name').text().trim())
-    //     let name = $(element).find('.quote-name').text().trim()
-    //     let pctChange = $(element).find('.quote-change').text().trim()
-    //     let symbol = $(element).find('a').attr('href')
-    //     symbol = symbol.slice(symbol.indexOf('=') + 1, )
-    //     let gainerItem = {
-    //         name: name,
-    //         pctChange: pctChange,
-    //         symbol: symbol
-    //     }
-
-    //     gainers.push(gainerItem)
-    //  });
-    // return gainers;
+ 
