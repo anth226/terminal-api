8cd24805322e2b05c0977e3cba5f36359b04018a
masterial <konstantin.k.yurchenko@gmail.com>
2020-03-18 14:23:49 -0700

    Implements portfolios endpoint with s3 holdings cache.

--- src/finbox/titans.js
+++ src/finbox/titans.js
@@ -1,58 +1,71 @@
-import axios from 'axios';
+import axios from "axios";
 
 export async function getAll() {
-    let portfolios = axios.get('https://makeshift.finbox.com/v4/ideas?limit=30&skip=0')
+  let portfolios = axios
+    .get("https://makeshift.finbox.com/v4/ideas?limit=30&skip=0")
     .then(function(res) {
-        return res
-    }).catch(function(err) {
-        return err
+      return res;
     })
+    .catch(function(err) {
+      return err;
+    });
 
-    return portfolios.then((data) => data.data)
+  return portfolios.then(data => data.data);
 }
 
 // export async function getPortfolios(investorTypes, ...sectors) {
-export async function getPortfolios({investorTypes, sectors, gte, lte}) {
-    let cleanInvestorType = investorTypes.charAt(0).toUpperCase() + investorTypes.substring(1);
+export async function getPortfolios({ investorTypes, sectors, gte, lte }) {
+  let cleanInvestorType =
+    investorTypes.charAt(0).toUpperCase() + investorTypes.substring(1);
 
-    // let paramFilters = {"filters":{"investor_types":[cleanInvestorType], "sectors": []},"limit":30,"skip":0}
-    let cleanSectors = [];
-    let sectorsArr;
+  // let paramFilters = {"filters":{"investor_types":[cleanInvestorType], "sectors": []},"limit":30,"skip":0}
+  let cleanSectors = [];
+  let sectorsArr;
 
-    // if (sectors.length > 0) {
-    //     sectorsArr = sectors.split(",")
-    //     for (let s of sectorsArr) {
-    //         let capitalized = s.charAt(0).toUpperCase() + s.substring(1)
-    //         cleanSectors.push(capitalized)
-    //     }
-        
-    let paramFilters = {"filters":{"asset_price_return_1y":{"$gte":gte,"$lte":lte}, "investor_types":[cleanInvestorType],"sectors": sectors},"limit":30,"skip":0}
-    // let paramFilters = {"filters":{"investor_types":[cleanInvestorType],"sectors": sectors},"limit":30,"skip":0}
+  // if (sectors.length > 0) {
+  //     sectorsArr = sectors.split(",")
+  //     for (let s of sectorsArr) {
+  //         let capitalized = s.charAt(0).toUpperCase() + s.substring(1)
+  //         cleanSectors.push(capitalized)
+  //     }
 
+  let paramFilters = {
+    filters: {
+      asset_price_return_1y: { $gte: gte, $lte: lte },
+      investor_types: [cleanInvestorType],
+      sectors: sectors
+    },
+    limit: 30,
+    skip: 0
+  };
+  // let paramFilters = {"filters":{"investor_types":[cleanInvestorType],"sectors": sectors},"limit":30,"skip":0}
 
-    // {"filters":{"asset_price_return_1y":{"$gte":-0.19999999999999993,"$lte":0.17999999999999983},"investor_types":["Billionaire"],"sectors":["Financials"]},"limit":30,"skip":0}
+  // {"filters":{"asset_price_return_1y":{"$gte":-0.19999999999999993,"$lte":0.17999999999999983},"investor_types":["Billionaire"],"sectors":["Financials"]},"limit":30,"skip":0}
 
-    let portfolios = axios.post('https://makeshift.finbox.com/v4/ideas/query', paramFilters)
+  let portfolios = axios
+    .post("https://makeshift.finbox.com/v4/ideas/query", paramFilters)
     .then(function(res) {
-        return res
-    }).catch(function(err) {
-        console.log(err)
-        return err
+      return res;
     })
+    .catch(function(err) {
+      console.log(err);
+      return err;
+    });
 
-    // console.log(portfolios.then((data) => data.data))
-    return portfolios.then((data) => data.data)
-
+  // console.log(portfolios.then((data) => data.data))
+  return portfolios.then(data => data.data);
 }
 // {"filters":{"investor_types":["Activist Investor"]},"limit":30,"skip":0}
 
-export async function getSinglePortfolioData(portfolioName){
-        let portfolio = axios.get(`https://makeshift.finbox.com/v4/ideas/${portfolioName}`)
-        .then(function(res) {
-            return res
-        }).catch(function(err) {
-            return err
-        })
-    
-        return portfolio.then((data) => data.data)
+export async function getSinglePortfolioData(portfolioName) {
+  let portfolio = axios
+    .get(`https://makeshift.finbox.com/v4/ideas/${portfolioName}`)
+    .then(function(res) {
+      return res;
+    })
+    .catch(function(err) {
+      return err;
+    });
+
+  return portfolio.then(data => data.data);
 }
