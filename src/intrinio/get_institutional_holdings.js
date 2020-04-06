2bbf4e5a938b21da7a854092141489c8c7f596fd
masterial <konstantin.k.yurchenko@gmail.com>
2020-04-06 15:09:31 -0700

    Implements scripts/endpoint for fetching titan holdings data.

--- src/intrinio/get_institutional_holdings.js
+++ src/intrinio/get_institutional_holdings.js
@@ -1,10 +1,17 @@
 import axios from "axios";
 
-export function getInstitutionalHoldings(cik) {
+// https://api-v2.intrinio.com/zacks/institutional_holdings?owner_cik=0001584087&page_size=10000&api_key=
+// https://api-v2.intrinio.com/zacks/institutional_holdings?owner_cik=0001584087&next_page=UlBBWXwzMDYxNzMxMDI&api_key=
+
+export function getInstitutionalHoldings(cik, next_page = null) {
+  let url = `https://api-v2.intrinio.com/zacks/institutional_holdings?owner_cik=${cik}&api_key=${process.env.INTRINIO_API_KEY}`;
+
+  if (next_page) {
+    url = `https://api-v2.intrinio.com/zacks/institutional_holdings?owner_cik=${cik}&next_page=${next_page}&api_key=${process.env.INTRINIO_API_KEY}`;
+  }
+
   let data = axios
-    .get(
-      `https://api-v2.intrinio.com/zacks/institutional_holdings?owner_cik=${cik}&api_key=${process.env.INTRINIO_API_KEY}`
-    )
+    .get(url)
     .then(function(res) {
       return res.data;
     })
