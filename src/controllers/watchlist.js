faf31449e84f78b2693e41edd6d12df86be22a47
masterial <konstantin.k.yurchenko@gmail.com>
2020-06-26 10:54:59 -0700

    Rehashes watchlist logic.

--- src/controllers/watchlist.js
+++ src/controllers/watchlist.js
@@ -2,10 +2,31 @@ import db from "../db";
 
 export const getFollowedTitans = async (userId) => {
   let result = await db(`
-      SELECT *
-      FROM billionaire_watchlists
-      WHERE user_id = '${userId}'
-    `);
+    SELECT billionaires.*, billionaire_watchlists.*
+    FROM billionaire_watchlists
+    LEFT JOIN billionaires
+    ON billionaire_watchlists.titan_id = billionaires.id
+    WHERE user_id = '${userId}'
+  `);
+
+  if (result.length > 0) {
+    let ciks = result.map(function (a) {
+      return a.cik;
+    });
+    let query = {
+      text: "SELECT * FROM institutions WHERE cik = ANY($1::text[])",
+      values: [ciks],
+    };
+
+    let buffer = await db(query);
+
+    result = buffer.map((x) =>
+      Object.assign(
+        x,
+        result.find((y) => y.cik == x.cik)
+      )
+    );
+  }
 
   return result;
 };
