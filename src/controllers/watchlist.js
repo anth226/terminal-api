5e0df6274803909f0b56f66ca7aa712ec058e095
masterial <konstantin.k.yurchenko@gmail.com>
2020-08-10 13:36:15 -0700

    Implements mutual fund watchlist logic.

--- src/controllers/watchlist.js
+++ src/controllers/watchlist.js
@@ -33,11 +33,37 @@ export const getFollowedTitans = async (userId) => {
   return result;
 };
 
-export const watching = async (id, userId) => {
+export const isWatching_Billionaire = async (id, userId) => {
   let result = await db(`
     SELECT *
     FROM billionaire_watchlists
     WHERE user_id = '${userId}' AND titan_id = '${id}'
+  `);
+
+  if (result.length > 0) {
+    return true;
+  }
+
+  return false;
+};
+
+export const getFollowedMutualFunds = async (userId) => {
+  let result = await db(`
+    SELECT mutual_funds.*, mutual_fund_watchlists.*
+    FROM mutual_fund_watchlists
+    LEFT JOIN mutual_funds
+    ON mutual_fund_watchlists.mutual_fund_id = mutual_funds.id
+    WHERE user_id = '${userId}'
+  `);
+
+  return result;
+};
+
+export const isWatching_MutualFund = async (id, userId) => {
+  let result = await db(`
+    SELECT *
+    FROM mutual_fund_watchlists
+    WHERE user_id = '${userId}' AND mutual_fund_id = '${id}'
   `);
 
   if (result.length > 0) {
