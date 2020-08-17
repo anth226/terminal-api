788664f675fa790478a8d45612d78266ec2e5945
Anthony Peterson <anthony@petersonrace.com>
2020-08-17 14:25:31 -0700

    Fixes route

--- src/index.js
+++ src/index.js
@@ -1199,7 +1199,7 @@ isAuthorized({ hasRole: ["admin"] }),
 // Mutual funds
 app.use("/mutual-fund/:identifier", checkAuth);
 app.get("/mutual-fund/:identifier", async (req, res) => {
-  const result = await mututal_funds.lookup(companyAPI, req.params.identifier);
+  const result = await mutual_funds.lookup(companyAPI, req.params.identifier);
   res.send(result);
 });
 
