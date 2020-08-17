3ea4a25a9268154ea3d38b61a75f12c746b00126
Reed Kilpatrick <reedk865@gmail.com>
2020-08-17 16:45:12 -0700

    savings api

--- src/scrape/nerdwallet_savings.js
+++ src/scrape/nerdwallet_savings.js
@@ -1,8 +1,20 @@
 import axios from 'axios';
-import cheerio from 'cheerio';
+//import cheerio from 'cheerio';
 
-const url = 'https://www.nerdwallet.com/best/banking/savings-accounts'
+const s3Savings = "https://terminal-scrape-data.s3.amazonaws.com/savings/accounts.json";
 
+export async function getSavingsAccounts() {
+  try {
+    const response = await axios.get(s3Savings);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+//const url = 'https://www.nerdwallet.com/best/banking/savings-accounts'
+
+/*
 const fetchDataSavingsAccounts = async () => {
     const result = await axios.get('https://www.nerdwallet.com/best/banking/savings-accounts');
     return cheerio.load(result.data);
@@ -12,18 +24,30 @@ export async function getSavingsAccountsList() {
     let data = []
     const $ = await fetchDataSavingsAccounts();
 
-    $('.B6cJf').first().find('tr').each(function(idx, element) {
+    $('table').first().find('tr').each(function(idx, element) {
         //console.log($(element).text().trim())
         //console.log($(element).find("a").attr("href"));
-        console.log($(element).find("picture").attr("src"));
+        let firstCol = $(element).find("td").first();
+        let bankImg = firstCol.find("img").attr("src");
+        let bankName = firstCol.find("p").text();
+
+        let apyCol = $(element).find("td:nth-child(3)");
+        let apy = apyCol.find("p").first().text();
+        let balance = apyCol.find("p").last().text();
+
+        let link = $(element).find("td").last().find("a").attr("href")
+
         const account = {
-            name: ($(element).find("div ._11bDt").text().trim())
+            name: bankName,
+            img: bankImg,
+            apy: apy,
+            balance: balance,
+            link: link,
         }
-        //console.log("----")
-        //console.log($(element).find("._2WhY_").text().trim())
-        //data.push($(element).text().trim())
+
+        data.push(account);
     })
 
     return data
 }
-
+*/
