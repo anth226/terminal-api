dda0fab1ae08889110ea11a4d7dddf401990b57a
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2020-08-18 16:53:42 -0700

    feat: add note column to billionaires

--- src/controllers/edgar.js
+++ src/controllers/edgar.js
@@ -43,7 +43,7 @@ export async function getCachedSearchResults({
       SELECT b.*, b_c.ciks
       FROM public.billionaires AS b
       LEFT JOIN (
-        SELECT titan_id, json_agg(json_build_object('cik', cik, 'name', name, 'is_primary', is_primary,'rank',rank) ORDER BY rank ASC) AS ciks
+        SELECT titan_id, json_agg(json_build_object('cik', cik, 'name', name, 'is_primary', is_primary, 'rank', rank) ORDER BY rank ASC) AS ciks
         FROM public.billionaire_ciks
         GROUP BY titan_id
     ) AS b_c ON b.id = b_c.titan_id) AS b
