71b7e31b76a03a8e4f28abc25203b51f7ed27ee9
masterial <konstantin.k.yurchenko@gmail.com>
2020-09-03 13:08:39 -0700

    Updates bot endpoint.

--- src/controllers/bots.js
+++ src/controllers/bots.js
@@ -1,18 +1,10 @@
 import axios from "axios";
 
 export async function processBillionaireSummary(billionaireId) {
-  // Set up queue for summary recalculation.
-
-  const url = "https://terminal-bot-staging.us-east-1.elasticbeanstalk.com/";
-
-  const data = { billionaireId };
+  const url = `https://terminal-bot-staging.us-east-1.elasticbeanstalk.com//billionaires/${billionaireId}/generate_summary?token=XXX`;
 
   try {
-    const response = await axios.post(url, data, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    const response = await axios.get(url);
     return { data: response.data };
   } catch (error) {
     return { data: null };
