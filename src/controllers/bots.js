93b71a517a1c502464454e6a533f82f5cabb39f6
masterial <konstantin.k.yurchenko@gmail.com>
2020-09-07 13:29:51 -0700

    Fixes bot endpoint.

--- src/controllers/bots.js
+++ src/controllers/bots.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export async function processBillionaireSummary(billionaireId) {
-  const url = `https://terminal-bot-staging.us-east-1.elasticbeanstalk.com/billionaires/${billionaireId}/generate_summary?token=XXX`;
+  const url = `http://terminal-bot-staging.us-east-1.elasticbeanstalk.com/billionaires/${billionaireId}/generate_summary?token=XXX`;
 
   try {
     const response = await axios.get(url);
