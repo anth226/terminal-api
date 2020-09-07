1f1509a11b35d2eab8eb9f17dbd6930d351203c0
masterial <konstantin.k.yurchenko@gmail.com>
2020-09-07 13:23:11 -0700

    FIxes process billionaires endpoint.

--- src/controllers/bots.js
+++ src/controllers/bots.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
 export async function processBillionaireSummary(billionaireId) {
-  const url = `https://terminal-bot-staging.us-east-1.elasticbeanstalk.com//billionaires/${billionaireId}/generate_summary?token=XXX`;
+  const url = `https://terminal-bot-staging.us-east-1.elasticbeanstalk.com/billionaires/${billionaireId}/generate_summary?token=XXX`;
 
   try {
     const response = await axios.get(url);
     return { data: response.data };
   } catch (error) {
+    console.log(error);
     return { data: null };
   }
 }
