32f3ab1a841f4874a4fc869398bd234ccdf749ea
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2020-10-16 10:22:38 -0700

    feat: build script for adding customers

---
+++ src/services/stripe.js
@@ -0,0 +1,12 @@
+import "dotenv/config";
+
+import Stripe from "stripe";
+
+export const couponId = process.env.STRIPE_COUPON_ID;
+export const planId = process.env.STRIPE_PLAN_ID;
+export const stripeKey = process.env.STRIPE_API_KEY;
+export const endpointSecret = process.env.STRIPE_ENDPOINT_SECRET;
+
+// export const endpointSecret = "whsec_VXvhsQM24tPVfEmCemSsSKNB6x3eL0z0";
+
+export const stripe = Stripe(stripeKey);
