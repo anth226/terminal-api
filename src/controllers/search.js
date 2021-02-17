ce9cdd993675dae017a1ef74ebc0d15feb8527fb
srbackendDev <maxwelldev0@gmail.com>
2021-02-17 11:20:11 -0600

    replace search endpoint with securities table

--- src/controllers/search.js
+++ src/controllers/search.js
@@ -1,51 +1,17 @@
 import db from "../db";
 import axios from "axios";
 
-export async function searchCompanies(intrinioApi, query, secApi) {
-  let [etfs, companyResults, mutualFunds, securities] = await Promise.all([
-    searchETF(secApi, query),
-    searchSec(intrinioApi, query),
-    searchMutualFunds(query),
-    searchSecurities(query),
-  ]);
-
-  etfs.forEach((etf, idx) => {
-    let etfTicker = etf.ticker;
-    etf.secType = "etf";
-    companyResults.companies.forEach(function (com, i) {
-      if (etfTicker == com.ticker) {
-        companyResults.companies.splice(i, 1);
-      }
-    });
+export async function searchCompanies(query) {
+  const securities = db({
+    text: `
+      SELECT ticker, name, type 
+      FROM securities
+      WHERE ticker LIKE '%' || $1 || '%'
+    `,
+    values: [query],
   });
 
-  mutualFunds.forEach((fund, i) => {
-    let fundTicker = fund.tickerSymbol;
-    fund.ticker = fund.tickerSymbol;
-    fund.secType = "mfund";
-    companyResults.companies.forEach(function (com, i) {
-      if (fundTicker == com.ticker) {
-        companyResults.companies.splice(i, 1);
-      }
-    });
-  });
-
-  securities.forEach((sec, i) => {
-    let securityTicker = sec.ticker;
-    companyResults.companies.forEach(function (com, i) {
-      if (securityTicker == com.ticker) {
-        companyResults.companies.splice(i, 1);
-      }
-    });
-  });
-
-  return interleaveArray(
-    interleaveArray(
-      interleaveArray(companyResults.companies, etfs),
-      mutualFunds
-    ),
-    securities
-  );
+  return securities;
 }
 
 export async function searchSecurities(query) {
@@ -95,40 +61,6 @@ export function searchSec(intrinioApi, query) {
   //console.log(res);
 
   return res;
-}
-
-async function searchETF(intrinioApi, query) {
-  const opts = {
-    pageSize: 30, // Number | The number of results to return
-  };
-
-  let res = intrinioApi
-    .searchSecurities(query, opts)
-    .then(function (data) {
-      return data.securities.filter(
-        (security) =>
-          security.currency == "USD" &&
-          security.code == "ETF" &&
-          security.composite_ticker.endsWith(":US")
-      );
-    })
-    .catch(function (error) {
-      return error;
-    });
-
-  return res;
-}
-
-function interleaveArray(array1, array2) {
-  let result = [];
-  let i,
-    l = Math.min(array1.length, array2.length);
-
-  for (i = 0; i < l; i++) {
-    result.push(array1[i], array2[i]);
-  }
-  result.push(...array1.slice(l), ...array2.slice(l));
-  return result;
 }
 
 export async function prefetchTitans() {
