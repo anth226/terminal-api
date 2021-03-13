66b6736149265022641b3abc2282019447867d95
anth226 <anthonyhutchi99@gmail.com>
2021-03-12 16:36:04 -0800

    block endpoint for prod

--- src/services/stripe.js
+++ src/services/stripe.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import Stripe from "stripe";
 
 export const couponId = process.env.STRIPE_COUPON_ID;
+export const couponIdFree = process.env.STRIPE_COUPON_ID_FREE;
 export const planId = process.env.STRIPE_PLAN_ID;
 export const yearlyPlanId = process.env.STRIPE_YEALY_PLAN_ID;
 export const stripeKey = process.env.STRIPE_API_KEY;
@@ -11,3 +12,4 @@ export const endpointSecret = process.env.STRIPE_ENDPOINT_SECRET;
 // export const endpointSecret = "whsec_VXvhsQM24tPVfEmCemSsSKNB6x3eL0z0";
 
 export const stripe = Stripe(stripeKey);
+
