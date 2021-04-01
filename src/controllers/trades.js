60ed548885e7cf2916676f5399829037fc5cebb8
tanNavarro <58436794+tanNavarro@users.noreply.github.com>
2021-04-02 00:53:27 +0800

    Update Alert to show Top 3 Market Values
    Update Alert to show Top 3 Market Values

--- src/controllers/trades.js
+++ src/controllers/trades.js
@@ -230,14 +230,14 @@ export async function getArchivedPortfolio(top5Only) {
 
 export async function getTop3Buy() {
   	const result = await db(`
-        SELECT * FROM daily_trades WHERE direction = 'Buy' AND created_at = (SELECT created_at FROM public.daily_trades ORDER by created_at DESC limit 1) ORDER BY SHARES DESC Limit 3
+        SELECT * FROM daily_trades WHERE direction = 'Buy' AND created_at = (SELECT created_at FROM public.daily_trades ORDER by created_at DESC limit 1) ORDER BY market_value DESC Limit 3
         `);
   	return result;
 }
 
 export async function getTop3Sell() {
   	const result = await db(`
-        SELECT * FROM daily_trades WHERE direction = 'Sell' AND created_at = (SELECT created_at FROM public.daily_trades ORDER by created_at DESC limit 1) ORDER BY SHARES DESC Limit 3
+        SELECT * FROM daily_trades WHERE direction = 'Sell' AND created_at = (SELECT created_at FROM public.daily_trades ORDER by created_at DESC limit 1) ORDER BY market_value DESC Limit 3
         `);
   	return result;
 }
