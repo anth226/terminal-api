c6d4ec550a3c8d6a734ab20b98ee07a7c8a0b15c
tanNavarro <58436794+tanNavarro@users.noreply.github.com>
2021-05-07 02:09:03 +0800

    remove extra line
    remove extra line

--- src/controllers/tiers.js
+++ src/controllers/tiers.js
@@ -6,4 +6,3 @@ export const displayActiveTierAndModule = async () => {
   FROM tiers t,feature_module fm, tiers_feature_module tfm
   WHERE t.id=tfm.tier_id AND fm.id=tfm.feature_module_id AND t.is_active = 'y' AND t.type IN ('a','m') GROUP BY t.name, t.type, t.price`);
 };
-
